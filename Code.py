b5d9b0525a6f458c578d4e27646620748343051d
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-09 08:47:00 +0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,3 +1,42 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# create a pandas DataFrame for the temperature and rainfall data
+data = pd.DataFrame({
+    'Month': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    '2020 Temperature': [21.2, 23.4, 25.6, 28.1, 31.4, 33.2, 34.5, 33.8, 31.5, 28.4, 25.1, 22.0],
+    '2020 Rainfall': [4.1, 4.5, 6.3, 20.1, 93.2, 228.3, 349.8, 330.1, 237.6, 82.3, 17.2, 5.9]
+})
+
+# create a combo chart with temperature and rainfall data across two years
+fig, ax1 = plt.subplots()
+
+# plot the temperature data on the left y-axis
+ax1.set_xlabel('Month')
+ax1.set_ylabel('Temperature (°C)')
+ax1.plot(data['Month'], data['2020 Temperature'], color='tab:blue', label='2020')
+ax1.tick_params(axis='y')
+
+# create a second y-axis on the right for the rainfall data
+ax2 = ax1.twinx()
+
+# plot the rainfall data on the right y-axis
+ax2.set_ylabel('Rainfall (mm)')
+ax2.bar(data['Month'], data['2020 Rainfall'], color='tab:green', alpha=0.5, label='2020')
+ax2.tick_params(axis='y')
+
+# add a legend to the chart
+fig.legend(loc='upper right')
+
+# set the chart title and show the chart
+plt.title('Monthly Temperature and Rainfall Data (2020)')
+plt.show()
+
+
+
+
+
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
